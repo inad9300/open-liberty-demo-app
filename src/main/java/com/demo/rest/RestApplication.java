0cab0df20f01ff730b7e1d01a014859afa367525
Daniel M <inad9300@gmail.com>
2023-03-10 09:28:20 +0100

    Add commented-out memory-leak-producing line

--- src/main/java/com/demo/rest/RestApplication.java
+++ src/main/java/com/demo/rest/RestApplication.java
@@ -3,5 +3,11 @@ package com.demo.rest;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.nio.ByteBuffer;
+
 @ApplicationPath("/api")
-public class RestApplication extends Application {}
+public class RestApplication extends Application {
+
+  // Uncomment to produce massive memory leak.
+  // static final ByteBuffer buf = ByteBuffer.allocate(100_000_000); // Allocate 100 MB.
+}
