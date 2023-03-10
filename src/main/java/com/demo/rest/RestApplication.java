8ae626f3a5fff40e378e7b63a07a0219d52a167d
Daniel M <inad9300@gmail.com>
2023-03-10 10:07:12 +0100

    Add commented-out code to produce NPE on startup

--- src/main/java/com/demo/rest/RestApplication.java
+++ src/main/java/com/demo/rest/RestApplication.java
@@ -1,5 +1,8 @@
 package com.demo.rest;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
@@ -10,4 +13,10 @@ public class RestApplication extends Application {
 
   // Uncomment to produce massive memory leak.
   // static final ByteBuffer buf = ByteBuffer.allocate(100_000_000); // Allocate 100 MB.
+
+  public void onInit(@Observes @Initialized(ApplicationScoped.class) Object __) {
+    Void npe = null;
+    // Uncomment to break Open Liberty's ability to restart upon source code changes.
+    // npe.toString();
+  }
 }
